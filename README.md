84344c00fe061fbc5d2e9ed05e790890239a663e
nimetts <105562039+nimetts@users.noreply.github.com>
2025-02-06 17:51:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# school_book_application
+ Teacher-Student version of our BookBuddy App
---
+++ android/app/src/main/java/com/example/school_book_application/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.school_book_application;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
